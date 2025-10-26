0af6e436ddd485ce7d30b4b8bf0770ea96cee501
masadnazir1 <masadnazir1@gmail.com>
2025-10-26 07:09:27 +0500

    fixed the issue with the home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,74 +1,10 @@
 "use client";
 import styles from "./../page.module.css";
 
-import HeroSection from "../components/sections/HeroSection/HeroSection";
-import Footer from "../components/sections/Footer/Footer";
-import ScrollToTop from "../components/sections/ScrollToTop/ScrollToTop";
-import Navbar from "../components/sections/Navbar/Navbar";
-
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Navbar />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <HeroSection />
-        <Footer
-          logo="DriveEasy"
-          links={[
-            { label: "About Us", href: "/about" },
-            { label: "Fleet", href: "/fleet" },
-            { label: "Services", href: "/services" },
-            { label: "Contact", href: "/contact" },
-            { label: "Blog", href: "/blog" },
-          ]}
-          socials={[
-            {
-              type: "facebook",
-              show: true,
-              href: "https://facebook.com/driveeasy",
-            },
-            {
-              type: "twitter",
-              show: true,
-              href: "https://twitter.com/driveeasy",
-            },
-            {
-              type: "linkedin",
-              show: true,
-              href: "https://linkedin.com/company/driveeasy",
-            },
-            {
-              type: "instagram",
-              show: true,
-              href: "https://instagram.com/driveeasy",
-            },
-          ]}
-          buttons={[
-            { label: "Book Now", type: "filled", href: "/booking" },
-            { label: "Subscribe", type: "outline", href: "/subscribe" },
-          ]}
-          showCopyright
-        />
-
-        <ScrollToTop />
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
